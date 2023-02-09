4a70684973eefaf7603445abd564bc96068c291a
ZZZIXUAN <zixuan.wang521@hotmail.com>
2023-02-09 23:51:15 +0100

    update

--- rank.py
+++ rank.py
@@ -1,7 +1,8 @@
 import json
 
+
 def get_document():
-    request = input('input your request:')
+    request = input('input your request:')  # lit une requête de l’utilisateur
     token_user = request.split()
     with open(r"index.json", 'r') as fp:
         index = json.loads(fp.read())
@@ -12,9 +13,9 @@ def get_document():
 
     sort = []
     filter_token = 0
-    for token in inter:
+    for token in inter:  # la tokenise et la transforme
         if token in ['to', 'with', 'on']:
-            filter_token = filter_token+1
+            filter_token = filter_token + 1
         else:
             print(token)
             document = index[token]
@@ -24,10 +25,10 @@ def get_document():
                 print(weight)
                 sort.append(weight)
             print('\n')
-    sort.sort(key=lambda x: x[1], reverse=True)
+    sort.sort(key=lambda x: x[1], reverse=True)  # fonction de ranking
 
     count = 0
-    with open('result.json', 'w') as fp:
+    with open('result.json', 'w') as fp:  # liste de retour
         for element in sort:
             document_index = int(element[0])
             title = documents[document_index]['title']
@@ -35,8 +36,8 @@ def get_document():
             print(title)
             print(url)
             print('---------------------------------------------------------')
-            count = count+1
+            count = count + 1
             fp.write(str({"title": title, "url": url, }) + "\n")
 
-    with open("metadata.json", "w") as fp:
-        fp.write(str({"TotalIndex": len(inter), "FilteredIndex": len(inter)-filter_token, "Total Records": count}))
+    with open("metadata.json", "w") as fp:  # metadata
+        fp.write(str({"Total Index": len(inter), "Filtered Index": len(inter) - filter_token, "Total Records": count}))
